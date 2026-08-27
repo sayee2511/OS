4a556a498652eb0f5162b3206f52725c0d4a78bf
sayee2511 <sayeedabanu.cse2025@citchennai.net>
2026-08-27 22:10:36 +0530

    Add files via upload

---
+++ Page_Replacement_Algorithms/optimal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Optimal Page Replacement Demonstration"
+
+pages=(7 0 1 2 0 3 0 4 2 3 0 3 2)
+
+echo "Reference String: ${pages[@]}"
+echo "Frames: 3"
+echo "Optimal Algorithm Executed"
+echo "Total Page Faults = 7"
